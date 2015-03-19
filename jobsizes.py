fdec712e553e170a0c1a49f7f26fa84919e9dd84
Anthony Scudese <anthony@robottalk.tv>
2015-03-19 18:36:00 -0400

    Added filtering via regex provided via the --filter= tag.

--- jobsizes.py
+++ jobsizes.py
@@ -1,5 +1,4 @@
 import scandir,re,os,sys,getopt,operator
-p = re.compile(ur'^\d{3}')
 
 def convert_bytes(bytes):
         bytes = float(bytes)
@@ -67,22 +66,26 @@ if __name__ == "__main__":
             elif opt in ("-f", "--filter"):
                 needle = arg
     jobs = {}
+    p = re.compile(needle)
     for top_dir in scandir.scandir(root_dir):
-        if (top_dir.is_dir(follow_symlinks=False) and re.search(p, top_dir.name)) or (needle is not None and top_dir.is_dir(follow_symlinks=False)):
+        if (top_dir.is_dir(follow_symlinks=False) and re.search(p, top_dir.name)) or (needle is None and top_dir.is_dir(follow_symlinks=False)):
             print "Calculating Job Sizes For: %s" % top_dir.name
             size = get_tree_size(os.path.join(root_dir, top_dir.name))
             jobs[top_dir.name] = size
-    max_name = max(len(s) for s in  jobs.keys())
-    row_format_tabbing = "{:<%d}" % (max_name + 10)
-    row_format =row_format_tabbing * 2
-    print row_format.format(*{'Job', 'Size'})
+    if len(jobs) > 0:
+        max_name = max(len(s) for s in  jobs.keys())
+        row_format_tabbing = "{:<%d}" % (max_name + 10)
+        row_format =row_format_tabbing * 2
+        print row_format.format(*{'Job', 'Size'})
 
-    if sort == 'name':
-        for job in sorted(jobs.items(), key=lambda (k,v): (k.lower(), v)):
-            print row_format.format(job[0], convert_bytes(job[1]))
-    elif sort == 'size':
-        for job in sorted(jobs.items(), key=operator.itemgetter(1), reverse=True):
-            print row_format.format(job[0], convert_bytes(job[1]))
+        if sort == 'name':
+            for job in sorted(jobs.items(), key=lambda (k,v): (k.lower(), v)):
+                print row_format.format(job[0], convert_bytes(job[1]))
+        elif sort == 'size':
+            for job in sorted(jobs.items(), key=operator.itemgetter(1), reverse=True):
+                print row_format.format(job[0], convert_bytes(job[1]))
+        else:
+            for job in jobs:
+                print row_format.format(job, convert_bytes(jobs[job]))
     else:
-        for job in jobs:
-            print row_format.format(job, convert_bytes(jobs[job]))
+        print "No Jobs Found or No Jobs matching the filter '%s'" % needle
