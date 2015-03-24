731c02ce52c1714dcb9392b7ff02b9aceb2b5d2e
Anthony Scudese <anthony@robottalk.tv>
2015-03-24 18:35:41 -0400

    added output flag

--- jobsizes_email.sh
+++ jobsizes_email.sh
@@ -6,4 +6,4 @@ MAILTO=$4
 MAILCC=$5
 NOW=$(date +"%m/%d/%y")
 
-python2.7 /usr/local/python/jobfoldersizes/jobsizes.py --root="$JOBSDIR" --sort=size --filter="$NEEDLE" | mail -s "Job Sizes as of $NOW" -r "$MAILFROM" -c "$MAILCC" "$MAILTO"
+python2.7 /usr/local/python/jobfoldersizes/jobsizes.py --root="$JOBSDIR" --sort=size --filter="$NEEDLE" --output=email | mail -s "Job Sizes as of $NOW" -r "$MAILFROM" -c "$MAILCC" "$MAILTO"
